848e2510985c8753ba2b3bc1add05f7296cb8819
Albina Gurung <albinakushu9545@gmail.com>
2024-05-05 21:37:18 +0545

    file upload successfull

--- app.js
+++ app.js
@@ -2,31 +2,47 @@
 const express=require('express')
 const app=express();//used to call functions
 //or const app=require('express')()
-
+app.use(express.static("/public/style.css"))
+const { blogs } = require('./model/index')
+require("./model/index")
 app.set('view engine','ejs')
-app.get('/',(req,res)=>{
-    const data={name:"albina" ,age:23}
-    res.render('home.ejs',{data})
-})
-
-app.get('/home',(req,res)=>
+// app.get('/blog',(req,res)=>{
+//     const data={name:"albina" ,age:23}
+//     res.render('home.ejs',{data})
+// })
+app.use(express.urlencoded({extended:true}))
+app.get('/',(req,res)=>
 {
     res.render('home.ejs')
 })
-app.get('/read',(req,res)=>
+app.get('/blog',(req,res)=>
 {
-    res.render('read.ejs')
+    res.render('blog.ejs')
 })
-app.get('/create',(req,res)=>
+app.get('/blog/home',(req,res)=>
+{
+    res.render('home.ejs')
+})
+
+app.get('/blog/create',(req,res)=>
 {
     res.render('create.ejs')
 })
-app.get('/blog/edit',(req,res)=>
+app.get('/blog/read',(req,res)=>
 {
-    res.render("edit.ejs")
+    res.render('read.ejs')
 })
-app.get('/blog',(req,res)=>{
-    res.render('blog.ejs')
+
+
+app.post('/blog',(req,res)=>{
+console.log(req.body)
+const {title,subtitle,description} = req.body 
+blogs.create({
+    title:title,
+    subTitle:subtitle,
+    description:description
+})
+res.redirect("/")
 })
 app.listen(3000,'127.0.0.1',()=>(
     console.log("server has started at port 3000")
