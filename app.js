66af8a79985b2522ce06fd9978e8e48931147411
Albina Gurung <albinakushu9545@gmail.com>
2024-05-06 19:42:19 +0545

    multer implemented plus redering data to view

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-
+const { blogs, sequelize } = require('./model/index')
+require("./model/index")
+const {multer,storage}=require('./middleware/multerConfig')
+const upload=multer({storage:storage})
 const express=require('express')
 const app=express();//used to call functions
 //or const app=require('express')()
-app.use(express.static("/public/style.css"))
-const { blogs } = require('./model/index')
-require("./model/index")
+app.use(express.static('public'));
+
 app.set('view engine','ejs')
+
 // app.get('/blog',(req,res)=>{
 //     const data={name:"albina" ,age:23}
 //     res.render('home.ejs',{data})
 // })
-app.use(express.urlencoded({extended:true}))
 app.get('/',(req,res)=>
 {
-    res.render('home.ejs')
+    res.render('home1.ejs')
 })
 app.get('/blog',(req,res)=>
 {
     res.render('blog.ejs')
 })
-app.get('/blog/home',(req,res)=>
-{
-    res.render('home.ejs')
-})
-
 app.get('/blog/create',(req,res)=>
 {
     res.render('create.ejs')
@@ -33,17 +30,78 @@ app.get('/blog/read',(req,res)=>
     res.render('read.ejs')
 })
 
+app.use(express.urlencoded({extended:true}));
 
-app.post('/blog',(req,res)=>{
-console.log(req.body)
-const {title,subtitle,description} = req.body 
-blogs.create({
-    title:title,
-    subTitle:subtitle,
-    description:description
+
+
+app.post('/blog',upload.single('Image'),(req,res)=>{
+    console.log(req.body)
+    const {title,subtitle,description} = req.body 
+    blogs.create({
+        title:title,
+        subTitle:subtitle,
+        description:description,
+        imageUrl:req.file.filename
+    })
+    res.redirect("/")
+    })
+
+ //sync the database and the model
+(async()=>{
+    try{
+     await sequelize.sync();
+     console.log("Database and model synced successfully")
+    }catch(error){
+console.error('error syncing the db and model',error)
+    }
+})();
+//fetching the data from the db and rendering it on the home1.ejs view
+app.get('/blog/home1',async(req,res)=>
+{
+   
+    try {
+        const allBlogs = await blogs.findAll();//making a rest call to an api to get data
+        console.log(allBlogs)
+       // res.json(allBlogs); // Assuming you want to display JSON data
+       console.log(res)
+      res.render('./views/home1.ejs',{allBlogs})
+    
+    } catch (error) {
+        console.error('Error fetching blogs:', error);
+        res.status(500).send('Internal server error');
+    }
 })
-res.redirect("/")
-})
+
+
+
+
 app.listen(3000,'127.0.0.1',()=>(
     console.log("server has started at port 3000")
 )); //It hosts my project at port 3000.
+
+
+
+//another way of connecting to database(old way)
+// const mysql=require('mysql2')
+// const connection=mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'',
+//     database:'node7db'
+
+// })
+// connection.connect((err)=>{
+// if(err) throw err;
+// console.log("Successfully Connected")});
+
+
+ 
+// const query="Select * from blogs";
+// connection.query(query,(err,results)=>{
+//     if(err) throw err;
+//     console.log('datafetched:'+results);
+// })
+
+ 
+
+ 
--- middleware/multerConfig.js
+++ middleware/multerConfig.js
@@ -6,7 +6,7 @@ const storage=multer.diskStorage({
     },
     filename:function(req,file,cb)
     {
-        cb(null,date.now() + '-' + file.originalname)
+        cb(null,Date.now() + '-' + file.originalname)
     }
 });
 module.exports={
